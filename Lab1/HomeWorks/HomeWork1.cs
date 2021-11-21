48e923178ad671ce146952087e6db50401ecd40d
Sopan <provie@mail.ru>
2021-11-21 18:10:33 +0300

    <Requred>

--- Lab1/HomeWorks/HomeWork1.cs
+++ Lab1/HomeWorks/HomeWork1.cs
@@ -13,7 +13,33 @@ namespace Lab1.HomeWorks
         /// <returns>Массив в котором поменяны местами максимальный отрицательный элемент и минимальный положительный</returns>
         public static int[] Variant1(int[] temp)
         {
-            throw new System.Exception("Не реализован вариант1");
+            int max = int.MaxValue;
+            int min = 0;
+            int minID = 0;
+            int maxID = 0;
+            int[] temp2 = temp;
+
+            for (int i = 0; i < temp2.Length; i++)
+            {
+                if (temp2[i] < 0 && temp2[i] < min)
+                {
+                    minID = i;
+                    min = temp2[i];
+                }
+                if (temp2[i] > 0 && temp2[i] < max)
+                {
+                    maxID = i;
+                    max = temp2[i];
+                }
+            }
+            if (min < 0 && max < int.MaxValue)
+            {
+                int a = temp2[minID];
+                temp2[minID] = temp2[maxID];
+                temp2[maxID] = a;
+                return temp2;
+            }
+            else return temp;
         }
 
         /// <summary>
@@ -23,7 +49,12 @@ namespace Lab1.HomeWorks
         /// <returns>Сумма элементов, состоящих на чётных позициях массива</returns>
         public static int Variant2(int[] temp)
         {
-            throw new System.Exception("Не реализован вариант2");
+            int sum = 0;
+            for(int i = 2; i < temp.Length; i+=2)
+            {
+                sum += temp[i];
+            }
+            return sum;
         }
 
         /// <summary>
@@ -33,7 +64,14 @@ namespace Lab1.HomeWorks
         /// <returns>Массив в котором поменяны отрицательные элементы на нули</returns>
         public static int[] Variant3(int[] temp)
         {
-            throw new System.Exception("Не реализован вариант3");
+           for(int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] < 0)
+                {
+                    temp[i] = 0;
+                }
+            }
+            return temp;
         }
 
         /// <summary>
@@ -43,7 +81,13 @@ namespace Lab1.HomeWorks
         /// <returns>Массив в котором *3 каждый положительный элемент, который стоит перед отрицательным</returns>
         public static int[] Variant4(int[] temp)
         {
-            throw new System.Exception("Не реализован вариант4");
+            for (int i = 1; i < temp.Length; i++){
+                if(temp[i] < 0 && temp[i-1] > 0)
+                {
+                    temp[i - 1] *= 3;
+                }
+            }
+            return temp;
         }
 
         /// <summary>
@@ -53,7 +97,19 @@ namespace Lab1.HomeWorks
         /// <returns>Разница между средним арифметическим и значение минимального элемента</returns>
         public static double Variant5(int[] temp)
         {
-            throw new System.Exception("Не реализован вариант5");
+            double sum = 0;
+            double min = temp[0];
+            for(int i = 0; i < temp.Length; i++)
+            {
+                sum += temp[i];
+                if(temp[i] <  min)
+                {
+                    min = temp[i];
+                }
+            }
+            double value = sum / temp.Length - min;
+            return value;
+
         }
     }
 }
--- Lab1/Tests/TestForHomeWork2.cs
+++ Lab1/Tests/TestForHomeWork2.cs
@@ -6,8 +6,8 @@ namespace Lab1.Tests
     {
         [TestCase(new string[] { "maxString", "world", "hi", "five", "two" }, "maxString", 9, "hi", 2)]
         [TestCase(new string[] { "mi", "rr", "ll", "ww", "kk" }, "kk", 2, "kk", 2)]
-        [TestCase(new string[] { "dd", "nw", "kw", "a", "" }, "a", 1, "", 0)]
-        [Ignore("Убрать после реализации метода")]
+        [TestCase(new string[] { "dd", "nw", "kw", "a", "" }, "kw", 2, "", 0)]
+        
         public void TestForVariant1(string[] temp, string max, int lengthMax, string min, int lengthMin) => Assert.AreEqual(HomeWorks.HomeWork2.Variant1(temp), (max, lengthMax, min, lengthMin),
         "Метод возвращает не верное значение");
 
